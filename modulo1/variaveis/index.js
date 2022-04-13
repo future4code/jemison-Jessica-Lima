11cf228604e6ad2dec546aaebff60714ced83719
jessica pereira <jessikapereira369@outlook.com>
2022-04-12 23:38:58 -0300

    exercicios-de-variaveis

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,53 @@
+// O valor neste momento de b é 10.
+let a = 10
+let b = 10
+console.log(b)
+/* O valor neste momento de a é 10, e o valor agora de b é 5. Com o comando de let, podemos alterar o valor da variavel a qualquer momento.*/
+b = 5
+console.log(a, b)
+
+// O valor de a, b e c é 10.
+let a = 10
+let b = 20
+c = a
+b = c
+a = b
+console.log(a, b, c)
+
+/* let p - let horas trabalhadas
+let t -let pagamento por dia*/ 
+let p = prompt("Quantas horas voce trabalha por dia?")
+let t = prompt("Quanto voce recebe por dia?")
+alert(`Voce recebe ${t/p} por hora`)
+
+1-a- let nome
+
+b- let idade
+console.log(typeof nome)
+console.log(typeof idade)
+
+c- undefined
+
+d- // O resultado foi undefined, por nao ter nenhum valor atribuído
+
+e- const nome = prompt("Qual é o seu nome?")
+let idade = prompt("Informe sua idade")
+
+f- // Notei que ao atribuir um valor, o comando typeof me deu o tipo das variaveis.
+const nome = "jessica"
+let idade = 22
+console.log(typeof nome)
+console.log(typeof idade)
+
+g- const nome = "jessica"
+let idade = 22
+console.log("Olá", nome, "você tem", idade, "anos.")
+
+2- let diaSemana = "SIM";
+console.log("Hoje é terça feira?", diaSemana)
+let projeto = "NAO";
+console.log("Hoje é dia de entrega do projeto de fixação?", projeto)
+let eEstudant = "SIM"
+console.log("Sou estudante da Labenu?", eEstudant)
+
+3- Infelizmente nao consegui entregar.
