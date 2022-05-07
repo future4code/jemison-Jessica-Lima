4729b57fca90cc1f12079fc13caea6787f3f3616
jessica pereira <jessikapereira369@outlook.com>
2022-05-06 21:37:57 -0300

    projeto blackjack

---
+++ modulo1/funcoes-de-array/index.js
@@ -0,0 +1,10 @@
+const usuarios = [
+    { nome: "Amanda Rangel", apelido: "Mandi" },
+    { nome: "Laís Petra", apelido: "Laura" },
+    { nome: "Letícia Chijo", apelido: "Chijo" }
+  ]
+  
+  const novoArrayA = usuarios.map((item, index, array) => {
+     console.log(item, index, array)
+  })
+  
