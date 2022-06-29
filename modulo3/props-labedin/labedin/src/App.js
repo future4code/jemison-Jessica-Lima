ac913bfb38d868bc33d989a9c643c3987e015fd0
jessica pereira <jessikapereira369@outlook.com>
2022-06-29 01:10:55 -0300

    trabalho-pronto

---
+++ modulo3/props-labedin/labedin/src/App.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import './App.css';
+import CardGrande from './components/CardGrande/CardGrande';
+import ImagemButton from './components/ImagemButton/ImagemButton';
+import CardPequeno from './components/CardPequeno/CardPequeno'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="page-section-container">
+        <h2>Dados pessoais</h2>
+        <CardGrande 
+          imagem="file:///C:/Users/ADMIN/OneDrive/Imagens/Fotos/Minhas/received_134898960683231.jpeg"
+          nome="Jéssica Pereira" 
+          descricao="Oi, eu sou a Jéssica. Sou aluna da labenu, estou apanhando muito para aprender os conteúdos, porém estou gostando bastante do curso de programação."
+        />
+        
+        <ImagemButton 
+          imagem="https://cdn-icons-png.flaticon.com/512/271/271210.png" 
+          texto="Ver mais"
+        />
+      </div>
+      <div> 
+        <CardPequeno 
+          imagem="file:///C:/Users/ADMIN/Downloads/o-email.png"
+          nome="Email"
+          descricao="jessikapereira369@outlook.com"
+        />  
+        
+        <CardPequeno
+          imagem="file:///C:/Users/ADMIN/Downloads/localizador-de-mapa.png"
+          nome="Endereço"
+          descricao="Rua Olimpio Rolim Loureiro"
+        />
+        </div>  
+     
+      <div className="page-section-container">
+        <h2>Experiências profissionais</h2>
+        <CardGrande 
+          descricao="Não tenho experiências profissionais!" 
+        />
+      </div>
+
+      <div className="page-section-container">
+        <h2>Minhas redes sociais</h2>
+        <ImagemButton 
+          imagem="https://d2v9ipibika81v.cloudfront.net/uploads/sites/261/2017/01/facebook-logo-3.png" 
+          texto="Facebook" 
+        />        
+
+        <ImagemButton 
+          imagem="https://logodownload.org/wp-content/uploads/2014/09/twitter-logo-1-1.png" 
+          texto="Twitter" 
+        />        
+      </div>
+   </div>
+  );
+}
+
+export default App;
---
+++ modulo3/props-labedin/labedin/src/components/CardPequeno/CardPequeno.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+function CardPequeno(props) {
+    return (
+        <div className="bigcard-container">
+            <img src={ props.imagem } />
+            <div>
+                <h4>{ props.nome }</h4>
+                <p>{ props.descricao }</p>
+            </div>
+        </div>
+    )
+}
+
+
+export default CardPequeno
